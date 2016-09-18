9b7ccd679571a16f11139896c47f1795d901dfc6
gt.drakeley@gmail.com <hw00xl_56851>
2016-09-17 19:33:41 -0700

    Partition ready for more robust testing

--- dungeon.py
+++ dungeon.py
@@ -10,49 +10,54 @@ class DungeonPartition(geometry.Rectangle):
         self.room = None
 
     def partition(self, depth):
-        # print('Partition at {}: x {}, y {}, xe {}, ye {}, w {}, h {}'.format(depth, self.x, self.y, self.xe, self.ye, self.w, self.h))
         if depth < 1:
-            print('Partition at {}: x {}, y {}, xe {}, ye {}, w {}, h {}'.format(depth, self.x, self.y, self.xe, self.ye, self.w, self.h))
-            # print("\tTerminating")
             return 1
-        elif depth & 0x1 and (True or random.random() < 0.5):
-            bound = self.center.x
+        elif random.random() < 0.5:
+            bound = self.w // 2
             self.fpart = DungeonPartition(self.x, self.y, bound, self.h)
-            self.spart = DungeonPartition(bound, self.y, self.w-bound, self.h)
-            # print('\tBounds: {}'.format(bound))
-            # print('\tVertical Split')
-            # print('\t\tFirst Partition: x {}, y {}, xe {}, ye {}, w {}, h {}'.format(self.fpart.x, self.fpart.y, self.fpart.xe, self.fpart.ye, self.fpart.w, self.fpart.h))
-            # print('\t\tSecond Partition: x {}, y {}, xe {}, ye {}, w {}, h {}'.format(self.spart.x, self.spart.y, self.spart.xe, self.spart.ye, self.spart.w, self.spart.h))
+            self.spart = DungeonPartition(self.x+bound, self.y, self.w-bound, self.h)
             return self.fpart.partition(depth-1) + self.spart.partition(depth-1)
         else:
-            bound = self.center.y
+            bound = self.h // 2
             self.fpart = DungeonPartition(self.x, self.y, self.w, bound)
-            self.spart = DungeonPartition(self.x, bound, self.w, self.h-bound)
-            # print('\tBounds: {}'.format(bound))
-            # print('\tHorizontal Split')
-            # print('\t\tFirst Partition: x {}, y {}, xe {}, ye {}, w {}, h {}'.format(self.fpart.x, self.fpart.y, self.fpart.xe, self.fpart.ye, self.fpart.w, self.fpart.h))
-            # print('\t\tSecond Partition: x {}, y {}, xe {}, ye {}, w {}, h {}'.format(self.spart.x, self.spart.y, self.spart.xe, self.spart.ye, self.spart.w, self.spart.h))
+            self.spart = DungeonPartition(self.x, self.y+bound, self.w, self.h-bound)
             return self.fpart.partition(depth-1) + self.spart.partition(depth-1)
 
     def fill_array(self, array, depth=0):
         if self.fpart:
             self.spart.fill_array(array, depth+1)
             self.fpart.fill_array(array, depth+1)
-        for x in range(self.x, self.xe):
-            array[self.y][x] = chr(65+depth)
-            # array[self.ye-1][x] = chr(65+depth)
-        for y in range(self.y, self.ye):
-            array[y][self.x] = chr(65+depth)
-            # array[y][self.xe-1] = chr(65+depth)
+        else:
+            for x in range(self.x+1, self.xe):
+                for y in range(self.y+1, self.ye):
+                    array[y][x] = ' '
 
+
+class DungeonFloor(DungeonPartition):
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
+
+    def partition(self, depth):
+        if depth < 1:
+            return 1
+        elif random.random() < 0.5:
+            bound = (self.w - 1) // 2
+            self.fpart = DungeonPartition(self.x, self.y, bound, self.h-1)
+            self.spart = DungeonPartition(self.x+bound, self.y, (self.w-1)-bound, self.h-1)
+            return self.fpart.partition(depth-1) + self.spart.partition(depth-1)
+        else:
+            bound = (self.h - 1) // 2
+            self.fpart = DungeonPartition(self.x, self.y, self.w-1, bound)
+            self.spart = DungeonPartition(self.x, self.y+bound, self.w-1, (self.h-1)-bound)
+            return self.fpart.partition(depth-1) + self.spart.partition(depth-1)
 
 if __name__ == '__main__':
     init = input('W, H, R: ').split()
-    d = DungeonPartition(0, 0, int(init[0]), int(init[1]))
+    d = DungeonFloor(0, 0, int(init[0]), int(init[1]))
     print('\n')
     print("Number of Rooms: ", d.partition(int(init[2])))
     print()
-    array = [[' ' for _ in range(d.w)] for _ in range(d.h)]
+    array = [['#' for _ in range(d.w)] for _ in range(d.h)]
     d.fill_array(array)
     s = '\n'.join(''.join(row) for row in array)
     print(s)
