536713ca1ff7597912cebde7c5f60d66061c614c
gt.drakeley@gmail.com <hw00xl_56851>
2016-09-19 23:30:50 -0700

    Correctly overloaded partition function for DungeonFloor class

--- dungeon.py
+++ dungeon.py
@@ -10,7 +10,9 @@ class DungeonPartition(geometry.Rectangle):
         self.spart = None
         self.feature = None
 
-    def partition(self, depth, mdim=(2, 2), bdim=(2, 2)):
+    def partition(self, depth, mdim=(2, 2), bdim=None):
+        if not bdim:
+            bdim = mdim
         if depth < 1:
             return 1
         elif self.w <= bdim[0] and self.h <= bdim[1]:
@@ -58,7 +60,39 @@ class DungeonFloor(DungeonPartition):
     def __init__(self, w, h):
         super().__init__(0, 0, w, h)
 
-    def partition(self, depth):
+    def partition(self, depth, mdim=(2, 2), bdim=None):
+        if not bdim:
+            bdim = mdim
+        if depth < 1:
+            return 1
+        elif self.w <= bdim[0] and self.h <= bdim[1]:
+            return 1
+        elif self.w <= mdim[0] * 2 and self.h <= mdim[1] * 2:
+            return 1
+        elif self.w <= mdim[0] * 2 or self.w <= bdim[0]:
+            bound = random.randint(mdim[1], self.h-mdim[1])
+            self.fpart = DungeonPartition(self.x, self.y, self.w-1, bound)
+            self.spart = DungeonPartition(self.x, self.y+bound, self.w-1, (self.h-1)-bound)
+        elif self.h <= mdim[1] * 2 or self.h <= bdim[1]:
+            bound = random.randint(mdim[0], self.w-mdim[0])
+            self.fpart = DungeonPartition(self.x, self.y, bound, self.h-1)
+            self.spart = DungeonPartition(self.x+bound, self.y, (self.w-1)-bound, self.h-1)
+        elif random.random() < 0.5:
+            # bound = self.w // 2
+            bound = random.randint(mdim[0], self.w-mdim[0])
+            self.fpart = DungeonPartition(self.x, self.y, bound, self.h-1)
+            self.spart = DungeonPartition(self.x+bound, self.y, (self.w-1)-bound, self.h-1)
+            # return self.fpart.partition(depth-1) + self.spart.partition(depth-1)
+        else:
+            # bound = self.h // 2
+            bound = random.randint(mdim[1], self.h-mdim[1])
+            self.fpart = DungeonPartition(self.x, self.y, self.w-1, bound)
+            self.spart = DungeonPartition(self.x, self.y+bound, self.w-1, (self.h-1)-bound)
+            # return self.fpart.partition(depth-1) + self.spart.partition(depth-1)
+        return self.fpart.partition(depth - 1, mdim, bdim) + self.spart.partition(depth - 1, mdim, bdim)
+
+    """
+    def partition(self, depth, ):
         if depth < 1:
             return 1
         elif random.random() < 0.5:
@@ -71,6 +105,7 @@ class DungeonFloor(DungeonPartition):
             self.fpart = DungeonPartition(self.x, self.y, self.w-1, bound)
             self.spart = DungeonPartition(self.x, self.y+bound, self.w-1, (self.h-1)-bound)
             return self.fpart.partition(depth-1) + self.spart.partition(depth-1)
+    """
 
 if __name__ == '__main__':
     init = input('W, H, R: ').split()
